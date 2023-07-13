cfc529394734024d432d73251619a01f3db8064f
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-13 20:30:27 -0300

    feat: add UserEmail VO

---
+++ src/Contexts/auth/domain/value-objects/UserEmail.ts
@@ -0,0 +1,29 @@
+import { StringValueObject } from "./StringValueObjects";
+
+export class UserEmail extends StringValueObject {
+	private readonly email: string;
+
+	constructor(value: string) {
+		super(value);
+		this.ensureMaxEmailLength(value);
+		this.ensureIsValidEmail(value);
+		this.email = value;
+	}
+
+	public getEmail(): string {
+		return this.email;
+	}
+
+	private ensureIsValidEmail(email: string): void {
+		const emailRegex = /\S+@\S+\.\S+/;
+		if (!emailRegex.test(email)) {
+			throw new Error("Invalid email.");
+		}
+	}
+
+	private ensureMaxEmailLength(email: string): void {
+		if (email.length > 125) {
+			throw new Error("Email should not exceed 125 characters.");
+		}
+	}
+}
