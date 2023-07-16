145832f6e47e7c4c6e111a48a2f58c7bbaf4d368
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-16 02:30:56 -0300

    feat: add login service

--- src/Contexts/auth/application/LoginService.ts
+++ src/Contexts/auth/application/LoginService.ts
@@ -1,1 +1,17 @@
-export class LoginService {}
+import { User } from "../domain/entities";
+import { UserRepository } from "../domain/repository/UserRepository";
+import { UserEmail, UserPassword } from "../domain/value-objects";
+
+export class LoginService {
+	protected repository;
+	constructor(repository: UserRepository) {
+		this.repository = repository;
+	}
+
+	async run(email: string, password: string): Promise<User> {
+		const userInstance = User.create(new UserEmail(email), new UserPassword(password));
+		const user = await this.repository.login(userInstance);
+
+		return user;
+	}
+}
