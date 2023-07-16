325ab7b90ecaf0f1b504bac632cafd278796d64d
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-16 02:31:40 -0300

    refactor: comment login with google/facebook methods

--- src/Contexts/auth/domain/repository/UserRepository.ts
+++ src/Contexts/auth/domain/repository/UserRepository.ts
@@ -1,7 +1,7 @@
 import { User } from "../entities";
 
 export interface UserRepository {
-	login(email: string, password: string): Promise<User>;
-	loginWithGoogle(email: string, googleId: string): Promise<User>;
-	loginWithFacebook(email: string, facebookId: string): Promise<User>;
+	login(user: User): Promise<User>;
+	//loginWithGoogle(email: string, googleId: string): Promise<User>;
+	//loginWithFacebook(email: string, facebookId: string): Promise<User>;
 }
