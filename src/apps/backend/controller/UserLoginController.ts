3c93e394270f6496fab69ff163a6e02e793c43ab
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-16 02:30:15 -0300

    feat: add user login controller

--- src/apps/backend/controller/UserLoginController.ts
+++ src/apps/backend/controller/UserLoginController.ts
@@ -1,3 +1,16 @@
-class UserLoginController {
-	async run(req: Request, res: Response) {}
+import { LoginService } from "../../../Contexts/auth/application";
+import { User } from "../../../Contexts/auth/domain/entities";
+
+export class UserLoginController {
+	protected login;
+
+	constructor(login: LoginService) {
+		this.login = login;
+	}
+
+	async __invoke(email: string, password: string): Promise<User> {
+		const user = await this.login.run(email, password);
+
+		return user;
+	}
 }
