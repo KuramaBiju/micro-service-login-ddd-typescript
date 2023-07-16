f6fba88733f51b4ffb1225987dd1bbe401c76816
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-16 02:31:11 -0300

    refactor: add fromPrimitives method

--- src/Contexts/auth/domain/entities/User.ts
+++ src/Contexts/auth/domain/entities/User.ts
@@ -12,4 +12,8 @@ export class User {
 	static create(email: UserEmail, password: UserPassword): User {
 		return new User(email, password);
 	}
+
+	static fromPrimitives(plainData: { email: string; password: string }): User {
+		return new User(new UserEmail(plainData.email), new UserPassword(plainData.password));
+	}
 }
