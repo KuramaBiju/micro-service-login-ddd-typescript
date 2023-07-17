27697e82b626bb075cc3cc5fb8ea425a418d2136
KuramaBiju <jeronimoledesma0@gmail.com>
2023-07-16 23:11:44 -0300

    refactor: add 2 methods in UserRepository interface

--- src/Contexts/auth/domain/repository/UserRepository.ts
+++ src/Contexts/auth/domain/repository/UserRepository.ts
@@ -1,7 +1,10 @@
+import { User as UserModel } from "../../infraestructure/models/User";
 import { User } from "../entities";
 
 export interface UserRepository {
 	login(user: User): Promise<User>;
+	findByEmail(email: string): Promise<UserModel>;
+	comparePassword(password: string, hash: string): Promise<void>;
 	//loginWithGoogle(email: string, googleId: string): Promise<User>;
 	//loginWithFacebook(email: string, facebookId: string): Promise<User>;
 }
